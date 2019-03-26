1b5ceb5119e6e39bfc5b11f3ddde772454834538
Your Name <you@example.com>
2019-03-26 20:38:10 -0300

    Clase02

--- Clase 02/index.php
+++ Clase 02/index.php
@@ -1,16 +1,59 @@
 <?php
 
 //Escribimos
-$vNameFile = 'archivo.txt';
+/* $vNameFile = 'archivo.txt';
 $vFile = fopen($vNameFile, 'a');
 $vData = 'Prueba datos <br>';
 fwrite($vFile, $vData);
-fclose($vFile);
+fclose($vFile); */
 
 // Leemos
-$vNameFile = 'archivo.txt';
+/* $vNameFile = 'archivo.txt';
 $vFile = fopen($vNameFile, 'r');
 $data = fread($vFile,filesize($vNameFile));
 fclose($vFile);
-echo $data;
+echo $data; */
+
+if(isset($_POST['texto'])) {
+  $vTexto = $_POST['texto'];
+  $listaNombre = [];
+  array_push($listaNombre, $vTexto);
+  agregarAlArchivo($listaNombre);
+  foreach ($listaNombre as $nombre) {
+   echo $nombre;
+  }
+} else {
+  echo leerArchivo();
+}
+
+function agregarAlArray($array, $texto){
+  array_push($array, $texto);
+}
+
+function agregarAlArchivo($array) {
+  $vNameFile = 'archivo.txt';
+  $vFile = fopen($vNameFile, 'a');
+  $vData = $array;
+  fwrite($vFile, $vData);
+  fclose($vFile);
+}
+
+function leerArchivoAArray(){
+  $vNameFile = 'archivo.txt';
+  $vFile = fopen($vNameFile, 'r');
+  //$data = fread($vFile,filesize($vNameFile));
+  $listaNombre = array();
+
+  $linea = fgets($vFile);
+  while(! feof($vFile))
+  {
+    array_push($listaNombre, $linea);
+    echo $linea;
+    $linea = fgets($vFile);
+  }
+
+  fclose($vFile);
+  return $listaNombre;
+}
+
 ?>
