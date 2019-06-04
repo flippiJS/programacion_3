3c66581c1afdfb9ea88b2316092d1995fa87c6a9
soyflippi <soyflippi@gmail.com>
2019-06-04 19:46:05 -0300

    method rest

--- Clase08/api/index.php
+++ Clase08/api/index.php
@@ -2,14 +2,29 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-require '../../vendor/autoload.php';
+require './vendor/autoload.php';
 
 $app = new \Slim\App;
 
-$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
-    $name = $args['name'];
-    $response->getBody()->write("Hello, $name");
-
+$app->get('/', function (Request $request, Response $response, array $args) {
+    $response->write("Api v1.0");
     return $response;
 });
+
+$app->post('/', function (Request $request, Response $response, array $args) {
+    $response->write("Api v1.0");
+    return $response;
+});
+
+$app->put('/', function (Request $request, Response $response, array $args) {
+    $response->write("Api v1.0");
+    return $response;
+});
+
+$app->delete('/', function (Request $request, Response $response, array $args) {
+    $response->write("Api v1.0");
+    return $response;
+});
+
+
 $app->run();
